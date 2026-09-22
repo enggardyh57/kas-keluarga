73cc817ef5b9d109563230075465514604e5c81c
enggardyh5 <enggardyah57@gmail.com>
2026-09-22 12:37:04 +0700

    fix: fix theme switching

--- application/views/auth/daftar.php
+++ application/views/auth/daftar.php
@@ -222,8 +222,7 @@
                         class="btn btn--primary auth-submit"
                         type="submit">
 
-                        Masuk
-
+                        Daftar
                         <svg viewBox="0 0 24 24">
                             <path d="M5 12h14M13 5l7 7-7 7" />
                         </svg>
